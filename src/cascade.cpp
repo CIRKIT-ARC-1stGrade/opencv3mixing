fbcd9ea0f9d81ef12afc340918879bf9a2ada4aa
tanacchi <q111026d@mail.kyutech.jp>
2017-10-10 18:10:40 +0900

    Test resive function

--- src/cascade.cpp
+++ src/cascade.cpp
@@ -31,25 +31,10 @@ CascadeDetector::CascadeDetector()
 void CascadeDetector::cascade(const sensor_msgs::ImageConstPtr& msg)
 {
   cv_bridge::CvImagePtr src_img{cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8)};
-  double scale = 4.0;
+  double scale = 2.0;
   cv::Mat small_img{cv::saturate_cast<int>(src_img->image.rows/scale), cv::saturate_cast<int>(src_img->image.cols/scale), CV_8UC1};
-  cv::resize(src_img->image, small_img, small_img.size(), 0, 0, cv::INTER_LINEAR);
-  cv::equalizeHist(small_img, small_img);
-  // std::string cascade_name{"opencv3mixing/cascades/scottie.xml"};
-  std::string cascade_name{"/home/tanacchi/works/opencv/src/opencv3mixing/cascades/face.xml"};
-  cv::CascadeClassifier cascade;
-  cascade.load(cascade_name);
-  std::vector<cv::Rect> scotties;
-  cascade.detectMultiScale(small_img, scotties, 1.1, 2, CV_HAAR_SCALE_IMAGE, cv::Size(30, 30));
-  cv::Point center;
-  int radius;
-  for (const auto& elem : scotties) {
-    center.x = cv::saturate_cast<int>((elem.x + elem.width*0.5)*scale);
-    center.y = cv::saturate_cast<int>((elem.y + elem.height*0.5)*scale);
-    radius = cv::saturate_cast<int>((elem.width + elem.height)*0.25*scale);
-    cv::circle(src_img->image, center, radius, cv::Scalar(80, 80, 255), 3, 8, 0);
-  }
-  image_pub_.publish(src_img->toImageMsg());
+  cv::resize(src_img->image, small_img, small_img.size(), 0, 0, cv::INTER_NEAREST);
+  image_pub_.publish(cv_bridge::CvImage{std_msgs::Header{}, "mono8", small_img}.toImageMsg());
 }
 
 int main(int argc, char** argv)
