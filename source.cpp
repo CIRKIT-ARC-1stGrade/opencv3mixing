bc2c2598d6c24eb53f84b57acc2b7e9a5384c035
marubashi11 <marubashi.yuuki641@mail.kyutech.jp>
2017-10-10 18:21:33 +0900

    Add executable-file

--- source.cpp
+++ source.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv)
 {
-  constexpr double ratio{6,3};
+  constexpr double ratio{6.3};
   const cv::Mat src_image{cv::imread(argv[1], 1)};
   cv::Mat small_image{cv::saturate_cast<int>(src_image.rows/ratio), cv::saturate_cast<int>(src_image.cols/ratio), CV_8UC3};
-  cv::resize(image, small_image, small_image.size(), 0, 0, cv::INTER_LINEAR);
-  cv::imwrite(argv[1], image);
+  cv::resize(src_image, small_image, small_image.size(), 0, 0, cv::INTER_LINEAR);
+  imwrite(argv[1], small_image);
   return 0;
 }
